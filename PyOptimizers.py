8a43e3289550091481374eeeab3e20ae12bc5731
André Sousa <lsousa.andre@gmail.com>
2019-10-19 18:22:43 +0100

    new commit1

--- PyOptimizers.py
+++ PyOptimizers.py
@@ -1,7 +1,7 @@
 import torch.optim as optimizers
 
 
-def get_SGD(lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0):
+def get_SGD(lr=lr, momentum=0, dampening=0, weight_decay=0):
     
     return optimizers.SGD(lr=lr, momentum=0, dampening=0, weight_decay=0)
 
